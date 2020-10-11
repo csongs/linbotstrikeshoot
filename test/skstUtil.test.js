19ac3e8261c33f3feb1a83fde03381fb9aada64f
Csongs <danieldu71427@gmail.com>
2020-10-11 13:42:59 +0800

    no message

--- test/skstUtil.test.js
+++ test/skstUtil.test.js
@@ -1,27 +1,25 @@
 const should = require('should');
 const skstUtil = require('../lib/skstUtil');
-const botModel = require('../model/BotDTO');
-
 
 describe('#test isPlayChoice', () => {
     // 測試文字: {事情} choice {選項1} {選項2}
     it('should return true when {事情} choice {選項1} {選項2} ', done => {
         let text = " {事情} choice {選項1} {選項2} ";
-        let ret = botModel.isPlayChoice(text);
+        let ret = skstUtil.isPlayChoice(text);
         ret.should.equal(true);
         done()
     })
     // 測試文字: choice {選項1} {選項2}
     it('should return true when choice {選項1} {選項2}', done => {
         let text = " {事情} choice {選項1} {選項2} ";
-        let ret = botModel.isPlayChoice(text);
+        let ret = skstUtil.isPlayChoice(text);
         ret.should.equal(true);
         done()
     })
 
     // 測是文字: null
     it('should return undefined when array is empty', done => {
-        const ret = botModel.isPlayChoice(null);
+        const ret = skstUtil.isPlayChoice(null);
         ret.should.equal(false);
         done()
     })
