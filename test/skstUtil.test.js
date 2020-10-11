78809da780a9e3c0049162d4b075de3033a4d25a
Csongs <danieldu71427@gmail.com>
2020-10-11 13:33:23 +0800

    fix test code

--- test/skstUtil.test.js
+++ test/skstUtil.test.js
@@ -1,26 +1,27 @@
-const should = require('should')
-const skstUtil = require('../lib/skstUtil')
+const should = require('should');
+const skstUtil = require('../lib/skstUtil');
+const botModel = require('../model/BotDTO');
 
 
 describe('#test isPlayChoice', () => {
     // 測試文字: {事情} choice {選項1} {選項2}
     it('should return true when {事情} choice {選項1} {選項2} ', done => {
-        let text=" {事情} choice {選項1} {選項2} ";
-        let ret = skstUtil.isPlayChoice(text);
+        let text = " {事情} choice {選項1} {選項2} ";
+        let ret = botModel.isPlayChoice(text);
         ret.should.equal(true);
         done()
     })
     // 測試文字: choice {選項1} {選項2}
     it('should return true when choice {選項1} {選項2}', done => {
-        let text=" {事情} choice {選項1} {選項2} ";
-        let ret = skstUtil.isPlayChoice(text);
+        let text = " {事情} choice {選項1} {選項2} ";
+        let ret = botModel.isPlayChoice(text);
         ret.should.equal(true);
         done()
     })
 
     // 測是文字: null
     it('should return undefined when array is empty', done => {
-        const ret = skstUtil.isPlayChoice(null);
+        const ret = botModel.isPlayChoice(null);
         ret.should.equal(false);
         done()
     })
@@ -30,19 +31,19 @@ describe('#test isPlayChoice', () => {
 describe('#test strContain', () => {
     // 測試文字: 'a' 'aa'
     it('should return true when strContain("a","aa") ', done => {
-        let ret = skstUtil.strContain('a','aa');
+        let ret = skstUtil.strContain('a', 'aa');
         ret.should.equal(false);
         done()
     })
     // 測試文字: 'aa' 'a'
     it('should return true when strContain("aa","a") ', done => {
-        let ret = skstUtil.strContain('aa','a');
+        let ret = skstUtil.strContain('aa', 'a');
         ret.should.equal(true);
         done()
     })
     // 測試文字: 'a' 'a'
     it('should return true when strContain("a","a") ', done => {
-        let ret = skstUtil.strContain('a','a');
+        let ret = skstUtil.strContain('a', 'a');
         ret.should.equal(true);
         done()
     })
