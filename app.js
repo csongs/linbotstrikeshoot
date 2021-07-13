0e6716996e145088784d81ff62e9fca02f859b20
Csongs <danieldu71427@gmail.com>
2021-07-13 08:40:49 +0800

    no message

--- app.js
+++ app.js
@@ -329,11 +329,11 @@ async function handleText(message, replyToken, source, userName) {
 	console.log(message.text);
 
 	//攻略url
-	let stageKeyword = skstUtil.getMonstrikeUrlStageStr(inputMsg);
+	let stageKeyword = skstUtil.getMonstrikeUrlStageStr(message.text);
 	//let stageUrl = await executeMonstrikeUrlStageStr(message.text, source, userName)
 	console.log("stageUrl:next");
 	if (stageKeyword != null) {
-		let msg = await skstUtil.generateTogetherGamewithUrl(inputMsg)
+		let msg = await skstUtil.generateTogetherGamewithUrl(message.text)
 		return client.replyMessage(
 			replyToken,
 			msg
